098213968895c12332af890e6ddf8a022d0485a0
Kyle <kyle.c.steely@gmail.com>
2017-04-21 17:35:25 -0400

    significant progress update - byyear works

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,24 @@
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+if (window.location.pathname.includes('byyear')) {
+  /* show the div named for the year parmeter */
+  var child = document.getElementById(urlParams['y']);
+  child.style.display = 'block';
+} else {
+  var child = document;
+}
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -11,9 +32,9 @@ function currentDiv(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("slide");
-  var y = document.getElementsByClassName("slide-meta");
-  var z = document.getElementsByClassName("series-thumb");
+  var x = child.getElementsByClassName("slide");
+  var y = child.getElementsByClassName("slide-meta");
+  var z = child.getElementsByClassName("series-thumb");
   //var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
@@ -30,3 +51,21 @@ function showDivs(n) {
   z[slideIndex-1].style.display = "block";
   //dots[slideIndex-1].className += " w3-opacity-off";
 }
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  // supprot swipe on carousel
+  var slides = document.getElementsByClassName('slide');
+  var hammered = [];
+  for (elem = 0; elem < slides.length; elem++) {;
+    hammered[elem] = new Hammer(slides[elem]);
+    hammered[elem].on("swipeleft", function(ev) {
+        plusDivs(1);
+    });
+    hammered[elem].on("swiperight", function(ev) {
+        plusDivs(-1);
+    });
+  }
+
+});
+
